d026e20dfd2c02acf0732af9fbfdcf3ee1499311
zhujinming6 <zhujinming6@hikvision.com.cn>
2020-04-29 20:15:08 +0800

    feat:增加demo

---
+++ hummer-demos/src/main/java/org/hummer/demo/cluster/ElectInitService.java
@@ -0,0 +1,30 @@
+package org.hummer.demo.cluster;
+
+import org.hummer.cluster.service.ConsulElectService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+/**
+ * @author zhujinming6
+ * @create 2020-04-29 19:43
+ * @update 2020-04-29 19:43
+ **/
+@Component
+public class ElectInitService implements CommandLineRunner {
+
+    @Resource
+    ConsulElectService consulElectService;
+
+    /**
+     * Callback used to run the bean.
+     *
+     * @param args incoming main method arguments
+     * @throws Exception on error
+     */
+    @Override
+    public void run(String... args) throws Exception {
+        consulElectService.work("hummer");
+    }
+}
