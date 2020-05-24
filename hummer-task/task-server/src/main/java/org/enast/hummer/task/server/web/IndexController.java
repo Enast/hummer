03e477a32c304b0bbbd805bfe8951d622318ae7e
HendSame <154903157@qq.com>
2020-05-24 11:08:45 +0800

    前端源码

--- hummer-task/task-server/src/main/java/org/enast/hummer/task/server/web/IndexController.java
+++ hummer-task/task-server/src/main/java/org/enast/hummer/task/server/web/IndexController.java
@@ -4,13 +4,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
-public class IndexController {
-
-    @RequestMapping("/")
-    public String index(){
-        ModelAndView mv = new ModelAndView("index");
-        return "index";
-    }
-}
+//@RestController
+//public class IndexController {
+//
+//    @RequestMapping("/")
+//    public String index(){
+//        ModelAndView mv = new ModelAndView("index");
+//        return "index";
+//    }
+//}
 
